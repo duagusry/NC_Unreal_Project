6e3b040bab95670dbe1d85f9d72c371e2cb32091
furyhunter <duagusry@gmail.com>
2018-08-05 20:19:27 +0900

    헤더파일에서 전방선언, 불필요한 코드 제거/정리, 변수 이름 정리

--- HOF/Source/HOF/Private/HOFWorldGameMode.cpp
+++ HOF/Source/HOF/Private/HOFWorldGameMode.cpp
@@ -38,18 +38,13 @@ void AHOFWorldGameMode::BeginPlay()
 	Super::BeginPlay();
 
 	GameInstance = Cast<UHOFGameInstance>(GetGameInstance());
-
-	Counter = 3;
-	GetWorldTimerManager().SetTimer(countDownHandle, this, &AHOFWorldGameMode::OnTimerTick, 1.0f, true);
-	
-	GameInstance->SetGamePlayState(EGameplayState::World);		// 이걸 따로 유지할 필요가..?
 	GetGameState<AHOFGameState>()->SetState(EHOFGameState::GAME_WORLD);
 
 	//리소스 로딩
 	g_CardEvent->Parse(FPaths::ProjectDir() + L"Resource/CardEvent.xml");
 	
 	//맵 이벤트는 일단 하드코딩. 이후 설정으로 빼서 파싱할 예정..
-	int32 mapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLOT_HEIGHT] = \
+	int32 MapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLOT_HEIGHT] = \
 		/*
 	{
 		{ 0, 1, 2, 0, -1, -1, -1, 0 },
@@ -69,9 +64,9 @@ void AHOFWorldGameMode::BeginPlay()
 	};
 
 	if (!GameInstance->HasTransferData())
-		InitWorld(mapEventInfo);
+		InitWorld(MapEventInfo);
 	else
-		LoadWorld(mapEventInfo);
+		LoadWorld(MapEventInfo);
 
 }
 
@@ -83,29 +78,27 @@ void AHOFWorldGameMode::Tick(float DeltaSeconds)
 void AHOFWorldGameMode::InitGameState()
 {
 	Super::InitGameState();
-	/*
-	AHOFGameState* const AHOFGameState = GetGameState<AHOFGameState>();
-	if (AHOFGameState)
+	auto GameState = GetGameState<AHOFGameState>();
+	if (GameState)
 	{
-		AHOFGameState->GameStart();
+		GameState->GameStart();
 	}
-	*/
 }
 
-void AHOFWorldGameMode::InitWorld(int32 mapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLOT_HEIGHT])
+void AHOFWorldGameMode::InitWorld(int32 MapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLOT_HEIGHT])
 {
 	FActorSpawnParameters SpawnInfo;
-	FRotator myRot(0.0f, 0.0f, 0.0f);
-	FVector myLoc(0.0f, 0.0f, 0.0f);
-	int32 cardIndex = 0;
+	FRotator MyRot(0.0f, 0.0f, 0.0f);
+	FVector MyLoc(0.0f, 0.0f, 0.0f);
+	int32 CardIndex = 0;
 
-	WorldBoard = GetWorld()->SpawnActor<AHOFWorldBoardActor>(AHOFWorldBoardActor::StaticClass(), myLoc, myRot, SpawnInfo);
+	WorldBoard = GetWorld()->SpawnActor<AHOFWorldBoardActor>(AHOFWorldBoardActor::StaticClass(), MyLoc, MyRot, SpawnInfo);
 	WorldBoard->InitMapInfo();
 	for (int i = 0; i < WORLD_SLOT_WIDTH; i++)
 	{
 		for (int j = 0; j < WORLD_SLOT_HEIGHT; j++)
 		{
-			WorldBoard->CreateCardAt(mapEventInfo[i][j], i, j, cardIndex);
+			WorldBoard->CreateCardAt(MapEventInfo[i][j], i, j, CardIndex);
 		}
 	}
 
@@ -114,75 +107,55 @@ void AHOFWorldGameMode::InitWorld(int32 mapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLO
 	WorldBoard->GetCardOn(0, 0).Visit();
 }
 
-void AHOFWorldGameMode::LoadWorld(int32 mapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLOT_HEIGHT])
+void AHOFWorldGameMode::LoadWorld(int32 MapEventInfo[WORLD_SLOT_WIDTH][WORLD_SLOT_HEIGHT])
 {
 	FActorSpawnParameters SpawnInfo;
-	FRotator myRot(0.0f, 0.0f, 0.0f);
-	FVector myLoc(0.0f, 0.0f, 0.0f);
-	int32 cardIndex = 0;
-	auto transferData = GameInstance->TransferData;
+	FRotator MyRot(0.0f, 0.0f, 0.0f);
+	FVector MyLoc(0.0f, 0.0f, 0.0f);
+	int32 CardIndex = 0;
+	auto TransferData = GameInstance->TransferData;
 
-	WorldBoard = GetWorld()->SpawnActor<AHOFWorldBoardActor>(AHOFWorldBoardActor::StaticClass(), myLoc, myRot, SpawnInfo);
-	WorldBoard->m_InitialEventArray = transferData.WorldBoardData.InitialEventArray;
+	WorldBoard = GetWorld()->SpawnActor<AHOFWorldBoardActor>(AHOFWorldBoardActor::StaticClass(), MyLoc, MyRot, SpawnInfo);
+	WorldBoard->m_InitialEventArray = TransferData.WorldBoardData.InitialEventArray;
 	for (int i = 0; i < WORLD_SLOT_WIDTH; i++)
 	{
 		for (int j = 0; j < WORLD_SLOT_HEIGHT; j++)
 		{
-			if(mapEventInfo[i][j] != -1)
-				WorldBoard->CreateCardAt(i, j, cardIndex, transferData.WorldBoardData.WorldSlotData);
+			if(MapEventInfo[i][j] != -1)
+				WorldBoard->CreateCardAt(i, j, CardIndex, TransferData.WorldBoardData.WorldSlotData);
 		}
 	}
 
-	BaseStructs::Position LastPosition = transferData.CurrentPosition;
+	BaseStructs::Position LastPosition = TransferData.CurrentPosition;
 	SpawnPawnOnBoard(LastPosition.x, LastPosition.y);
 	InitWorldBoardPosition(LastPosition.x, LastPosition.y);
-	WorldBoard->GetCardOn(LastPosition.x, LastPosition.y).Visit(transferData.CurrentDialogId);
-}
-
-void AHOFWorldGameMode::OnTimerTick()
-{
-	//--Counter;
-
-	//타이머 만료시 BattleLevel 오픈
-	/*
-	if (Counter < 1)
-	{
-		g_PlayerData->EatFood();
-
-		UE_LOG(LogClass, Warning, TEXT("Alive? %s"), g_PlayerData->IsAlive() ? TEXT("Alive") : TEXT("Dead"));
-		UE_LOG(LogClass, Warning, TEXT("Count %d"), Counter);
-		GetWorld()->ServerTravel(FString("/Game/Maps/HOFBattleLevel"));
-		GetWorldTimerManager().ClearTimer(countDownHandle);
-	}
-	*/
+	WorldBoard->GetCardOn(LastPosition.x, LastPosition.y).Visit(TransferData.CurrentDialogId);
 }
 
 void AHOFWorldGameMode::MovePawnTo(int32 x, int32 y)
 {
-	BaseStructs::Position oldPosition = WorldPawn->GetPosition();
-	WorldBoard->UpdateAdjacentList(oldPosition.x, oldPosition.y, x, y);
+	BaseStructs::Position OldPosition = WorldPawn->GetPosition();
+	WorldBoard->UpdateAdjacentList(OldPosition.x, OldPosition.y, x, y);
 
-	FVector targetCardLocation = WorldBoard->GetCardLocationOn(x, y);
+	FVector TargetCardLocation = WorldBoard->GetCardLocationOn(x, y);
 
 	WorldPawn->SetPosition(x, y);
-	WorldPawn->SetRotation(targetCardLocation);
-	WorldPawn->SetLocation(targetCardLocation);
+	WorldPawn->SetRotation(TargetCardLocation);
+	WorldPawn->SetLocation(TargetCardLocation);
 }
 
-void AHOFWorldGameMode::HandleRevealEvent(int32 amount)
+void AHOFWorldGameMode::HandleRevealEvent(int32 Amount)
 {
-	WorldBoard->Reveal(amount);
+	WorldBoard->Reveal(Amount);
 }
 
 void AHOFWorldGameMode::SpawnPawnOnBoard(int32 x, int32 y)
 {
 	FActorSpawnParameters SpawnInfo;
-	FRotator myRot(0.0f, 0.0f, 0.0f);
-	FVector myLoc(0.0f, 0.0f, 0.0f);
-
+	FRotator MyRot(0.0f, 0.0f, 0.0f);
 	FVector PawnInitialLocaiton = WorldBoard->GetCardLocationOn(x, y);
 
-	WorldPawn = GetWorld()->SpawnActor<AHOFWorldPawn>(BP_WorldPawn, PawnInitialLocaiton, myRot, SpawnInfo);
+	WorldPawn = GetWorld()->SpawnActor<AHOFWorldPawn>(BP_WorldPawn, PawnInitialLocaiton, MyRot, SpawnInfo);
 	WorldPawn->SetPosition(x, y);
 }
 
@@ -191,6 +164,18 @@ void AHOFWorldGameMode::InitWorldBoardPosition(int32 x, int32 y)
 	WorldBoard->InitWorldStatus();
 	WorldBoard->InitAdjacentList();
 	WorldBoard->UpdateAdjacentList(0, 0, x, y);
+}
+
+BaseStructs::TransferData AHOFWorldGameMode::AssignTransferData()
+{
+	return BaseStructs::TransferData
+		{ BaseStructs::TransferData::WorldBoardDataStruct
+			{ WorldBoard->SerializeWorldSlotData(), 
+			WorldBoard->m_InitialEventArray 
+			}, 
+			WorldPawn->GetPosition(), 
+			0 
+		};
 }
 
 
